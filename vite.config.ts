2aa8864ba06b87c8a9a41402659ecf77cdeb2098
j0hnbarbosa <john.contatob@gmail.com>
2024-05-25 15:55:51 -0300

    fix base vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'http://agendamentoservicos.iconfiteam.com/',
   plugins: [react()],
   server: {
     watch: {
