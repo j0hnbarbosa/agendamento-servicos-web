51301d075a9b3db65d0cabc1a75b5033d7a8de21
j0hnbarbosa <john.contatob@gmail.com>
2025-05-01 23:04:13 -0300

    refact: Add default value to base

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
   
   return defineConfig({
-    base: process.env.VITE_BASE_URL,
+    base: process.env.VITE_BASE_URL || 'web-agenda',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
